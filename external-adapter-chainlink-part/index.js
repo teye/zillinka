a1616051dc2a84da09e9c8d6c21fc0ac94bf240b
TheDrBee <drbee@gmx.net>
2021-02-12 16:01:22 +0100

    write unix time received from api to a contract SimpleReceiver on the zilliqa bc

--- external-adapter-chainlink-part/index.js
+++ external-adapter-chainlink-part/index.js
@@ -1,4 +1,18 @@
+/* To run the curl request to get  unixtime
+
+/external-adapter-chainlink-part$ yarn
+  yarn run v1.22.5
+  $ node app.js
+  Listening on port 8080!
+# in a different terminal
+/external-adapter-chainlink-part$ curl -X POST -H "content-type:application/json" "http://localhost:8080/" --data '{ "id": 0, "data": { } }'
+  {"jobRunID":0,"data":{"abbreviation":"CET","client_ip":"85.5.161.167","datetime":"2021-02-12T12:07:21.611838+01:00","day_of_week":5,"day_of_year":43,"dst":false,"dst_from":null,"dst_offset":0,"dst_until":null,"raw_offset":3600,"timezone":"Europe/Berlin","unixtime":1613128041,"utc_datetime":"2021-02-12T11:07:21.611838+00:00","utc_offset":"+01:00","week_number":6,"result":1613128041},"result":1613128041,"statusCode":200}drbee@XPS-13-7390-cerchia:~/dev/gh-zillinka/external-adapter-chainlink-part$
+*/
+
 const { Requester, Validator } = require('@chainlink/external-adapter')
+const {  getPubKeyFromPrivateKey } = require('@zilliqa-js/crypto');
+const { BN, Long, units } = require('@zilliqa-js/util');
+const { setup_chain_and_wallet, call_contract} = require('./utils_zil.js')
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -42,18 +56,71 @@ const createRequest = (input, callback) => {
     params
   }
 
+  // FMBzilliqa bc stuff : get from curl parameter later
+  const use_testnet = false;
+  const receiver_address = "0xf3f162e733ab3fd5cae72fc1b9eb89355c671b46"; // where smart contract is on chain
+  const bc_setup = setup_chain_and_wallet(use_testnet); // which chain to use, fill wallet, etc
+  const priv_key = bc_setup.keys[0];
+  const pub_key = getPubKeyFromPrivateKey(priv_key);
+  bc_setup.zilliqa.wallet.addByPrivateKey(priv_key);
+  const receiver_sc = bc_setup.zilliqa.contracts.at(receiver_address);
+  const gas_price = units.toQa('5000', units.Units.Li);
+  const gas_limit = Long.fromNumber(50000);
+  const attempts = Long.fromNumber(10);
+
+
   // The Requester allows API calls be retry in case of timeout
   // or connection failure
   Requester.request(config, customError)
     .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-//      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
       response.data.result = Requester.validateResultNumber(response.data, ["unixtime"]) // FMB: extract unixtime from json
-      // TODO: FMB Take the result and send it to Zilliqa oracle contract
-      callback(response.status, Requester.success(jobRunID, response))
+      callback(response.status, Requester.success(jobRunID, response));
+
+      // FMB: Take the result and send it to Zilliqa oracle contract
+      const uxt = response.data.result;
+      console.log(` ===> unix time is ${uxt}`);
+      return new Promise(function(resolve, reject) {
+        if (uxt > 0) {
+          resolve(uxt);
+        }
+        else {
+          reject('unix time not positive')
+        }
+      });
     })
+    .then( uxt => { // set up the chain and wallet and load the contract from chain
+      console.log(` ===> calling set() with the unix time ${uxt} to write to contract @  ${receiver_sc.address}`);
+
+      const tx_settings = {   // use same settings for all transactions
+        "gas_price": units.toQa('5000', units.Units.Li),
+        "gas_limit": Long.fromNumber(50000),
+        "attempts": Long.fromNumber(10),
+      };
+      const args = [ { vname: 'data',  type: 'Uint128', value: uxt.toString() } ];
+      return call_contract(receiver_sc, 'set', args, new BN(0), pub_key, bc_setup, tx_settings);
+/*      return receiver_sc.call(
+        'set',
+         args,
+        { version: bc_setup.VERSION, amount: new BN(0), gasPrice: tx_settings.gas_price,
+          gasLimit: tx_settings.gas_limit, pubKey: pub_key, },
+          tx_settings.attempts, 1000, false,
+      );
+*/
+    })
+    .then( (tx) => {
+      if (tx.receipt.success) {
+        console.log(` ====> tx successful: querying state`);
+        return receiver_sc.getState();
+      }
+      else {
+        console.log(` ====> tx NOT successful`);
+      }
+    })
+    .then( (state) => {
+      console.log(" ====> state of contract after having called set(.) is: ")
+      console.log(state)
+    })
+    .then( )
     .catch(error => {
       callback(500, Requester.errored(jobRunID, error))
     })
