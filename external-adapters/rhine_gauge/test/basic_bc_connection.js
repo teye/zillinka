4fc2f2737feef547e7cf2b3bae792c422e148ce8
TheDrBee <drbee@gmx.net>
2021-03-15 12:08:07 +0100

    basic b/c setup and account test w/o requiring utils_zil.js

--- external-adapters/rhine_gauge/test/basic_bc_connection.js
+++ external-adapters/rhine_gauge/test/basic_bc_connection.js
@@ -1,20 +1,17 @@
 /* test connection to block chain and set up of account */
 var assert = require('assert');
-const {bc_secrets} = require("../../../secrets/blockchain.js");
-
-const {setup_chain_and_wallet} = require("../../commons/utils_zil.js");
-
-
+const { Zilliqa } = require('@zilliqa-js/zilliqa');
 const { getAddressFromPrivateKey } = require('@zilliqa-js/crypto');
 const { BN, units } = require('@zilliqa-js/util');
+const {bc_secrets} = require("../../../secrets/blockchain.js");
 
 describe("Basic connection to blockchain and set up of account", function () {
 
   it("should be able to get the balances on testnet and should be non-zero", async function() {
     const key = bc_secrets.privateKey;
-    const setup = setup_chain_and_wallet(true);
+    const zilliqa_chain = new Zilliqa(bc_secrets.zilliqa.api);
     const addr = getAddressFromPrivateKey(key);
-    const balance = await setup.zilliqa.blockchain.getBalance(addr);
+    const balance = await zilliqa_chain.blockchain.getBalance(addr);
     let b_zil = new BN(balance.result.balance);
     b_zil = units.fromQa(b_zil, 'zil');
     console.log(`  ... > balance of ${addr}: ${b_zil} ZIL`);
--- secrets/blockchain.js
+++ secrets/blockchain.js
@@ -1,7 +1,22 @@
 const bc_secrets =
 {
+  /* Zilliqa Testnet Setup */
   "privateKey":  // address: 0x56A7812f68cbF83194a4a777D2310Aa7A378C9D8 / zil126ncztmge0urr99y5amayvg2573h3jwcxgkqjv
     'b74501e0d2d047e8aaa2353020b46f31d396f92d05843665573300995e3aed88',
+
+  "zilliqa": { // blockchain
+    "api": 'https://dev-api.zilliqa.com',
+    "chainId": 333,
+    "msgVersion": 1,
+  },
+
+  "contracts": { // addresses of deployed smart contracts
+    "UnixTimeOracle": '0xaa28674d160a7b74cd6b3eedce733ac5c01cd26a',
+    "UnixTimeOracleClient": '0x61b0e75ad884ccb3253c9eaa6396dea3c80f2f67',
+    "RhineGaugeOracle": '0xbd0a71b5490291cf99b00a733068627abdac6fcc',
+    "RhineGaugeOracleClient": '0xc9357c221cebf5f20e35153103032747a0d99bbf',
+  },
+
 }
 
 exports.bc_secrets = bc_secrets
